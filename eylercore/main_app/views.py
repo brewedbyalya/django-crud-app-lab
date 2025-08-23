f96fe06182c50d3cff3e2d7e1f637366a4bb078f
alya <aliya2002mah@gmail.com>
2025-08-23 23:17:05 +0300

    Some errors. Mostly working. CSS untouched, needs work.

--- eylercore/main_app/views.py
+++ eylercore/main_app/views.py
@@ -2,75 +2,124 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib import messages
+
 from .models import Project, Task, Tag
 from .forms import ProjectForm, TaskForm, TagForm
 
+# Home page view
 def home(request):
     return render(request, 'main_app/home.html')
 
+# Authentication views
 def signup(request):
-    error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('main_app:projects_index')
-        else:
-            error_message = 'Invalid sign up - try again'
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+            return redirect('main_app:home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
-# Project Views
+# Project views
 @login_required
 def projects_index(request):
     projects = Project.objects.filter(user=request.user)
-    return render(request, 'main_app/projects_index.html', {'projects': projects})
+    return render(request, 'projects_index.html', {'projects': projects})
 
 @login_required
 def projects_detail(request, project_id):
     project = get_object_or_404(Project, id=project_id, user=request.user)
-    return render(request, 'main_app/projects_detail.html', {'project': project})
+    return render(request, 'projects_detail.html', {'project': project})
 
 @login_required
 def add_project(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
-            new_project = form.save(commit=False)
-            new_project.user = request.user
-            new_project.save()
-            return redirect('main_app/projects_index.html')
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+            return redirect('main_app:projects_index')
     else:
         form = ProjectForm()
-    return render(request, 'main_app/add_project.html', {'form': form})
+    return render(request, 'add_project.html', {'form': form})
 
 @login_required
 def edit_project(request, project_id):
     project = get_object_or_404(Project, id=project_id, user=request.user)
     if request.method == 'POST':
         form = ProjectForm(request.POST, instance=project)
-        if form.is_valid():
+        if form.isvalid():
             form.save()
-            return redirect('main_app/projects_detail.html', project_id=project.id)
+            return redirect('main_app:project_detail', project_id=project.id)
     else:
         form = ProjectForm(instance=project)
-    return render(request, 'main_app/edit_project.html', {'form': form, 'project': project})
+    return render(request, 'edit_project.html', {'form': form, 'project': project})
 
 @login_required
 def delete_project(request, project_id):
     project = get_object_or_404(Project, id=project_id, user=request.user)
     if request.method == 'POST':
         project.delete()
-        return redirect('main_app/projects_index')
-    return render(request, 'main_app/delete_project.html', {'project': project})
+        return redirect('main_app:projects_index')
+    return render(request, 'delete_project.html', {'project': project})
 
-# Tag Views
+# Task views
+@login_required
+def tasks_index(request, project_id):
+    project = get_object_or_404(Project, id=project_id, user=request.user)
+    tasks = Task.objects.filter(project=project)
+    return render(request, 'tasks/index.html', {'tasks': tasks, 'project': project})
+
+@login_required
+def task_detail(request, project_id, task_id):
+    task = get_object_or_404(Task, id=task_id, project__user=request.user)
+    return render(request, 'tasks/detail.html', {'task': task})
+
+@login_required
+def add_task(request, project_id):
+    project = get_object_or_404(Project, id=project_id, user=request.user)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.project = project
+            task.save()
+            # Redirect to project detail instead of task detail to avoid issues
+            return redirect('main_app:project_detail', project_id=project.id)
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/form.html', {'form': form, 'project': project})
+
+@login_required
+def edit_task(request, project_id, task_id):
+    task = get_object_or_404(Task, id=task_id, project__user=request.user)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            # Redirect to task detail after successful edit
+            return redirect('main_app:task_detail', project_id=task.project.id, task_id=task.id)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/form.html', {'form': form, 'task': task})
+
+@login_required
+def delete_task(request, project_id, task_id):
+    task = get_object_or_404(Task, id=task_id, project__user=request.user)
+    if request.method == 'POST':
+        project_id = task.project.id
+        task.delete()
+        return redirect('main_app:project_detail', project_id=project_id)
+    return render(request, 'tasks/delete.html', {'task': task})
+
+# Tag views
 @login_required
 def tags_index(request):
-    tags = Tag.objects.all()
+    tags = Tag.objects.filter(user=request.user)
     return render(request, 'tags/index.html', {'tags': tags})
 
 @login_required
@@ -78,89 +127,30 @@ def add_tag(request):
     if request.method == 'POST':
         form = TagForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('tags_index')
+            tag = form.save(commit=False)
+            tag.user = request.user
+            tag.save()
+            return redirect('main_app:tags_index')  # Fixed namespace
     else:
         form = TagForm()
-    return render(request, 'tags/form.html', {'form': form, 'action': 'Add'})
+    return render(request, 'tags/form.html', {'form': form})
 
 @login_required
 def edit_tag(request, tag_id):
-    tag = get_object_or_404(Tag, id=tag_id)
+    tag = get_object_or_404(Tag, id=tag_id, user=request.user)
     if request.method == 'POST':
         form = TagForm(request.POST, instance=tag)
         if form.is_valid():
             form.save()
-            return redirect('tags_index')
+            return redirect('main_app:tags_index')  # Fixed namespace
     else:
         form = TagForm(instance=tag)
-    return render(request, 'tags/form.html', {'form': form, 'action': 'Edit', 'tag': tag})
+    return render(request, 'tags/form.html', {'form': form, 'tag': tag})
 
 @login_required
 def delete_tag(request, tag_id):
-    tag = get_object_or_404(Tag, id=tag_id)
+    tag = get_object_or_404(Tag, id=tag_id, user=request.user)
     if request.method == 'POST':
         tag.delete()
-        return redirect('tags_index')
+        return redirect('main_app:tags_index')  # Fixed namespace
     return render(request, 'tags/delete.html', {'tag': tag})
-
-@login_required
-def tasks_index(request, project_id):
-    project = get_object_or_404(Project, id=project_id, user=request.user)
-    tasks = project.tasks.all()
-    return render(request, 'tasks/index.html', {'project': project, 'tasks': tasks})
-
-@login_required
-def task_detail(request, project_id, task_id):
-    project = get_object_or_404(Project, id=project_id, user=request.user)
-    task = get_object_or_404(Task, id=task_id, project=project)
-    return render(request, 'tasks/detail.html', {'project': project, 'task': task})
-
-@login_required
-def add_task(request, project_id):
-    project = get_object_or_404(Project, id=project_id, user=request.user)
-    if request.method == 'POST':
-        form = TaskForm(request.POST, project=project)
-        if form.is_valid():
-            task = form.save(commit=False)
-            task.project = project
-            task.save()
-            form.save_m2m() 
-            return redirect('tasks_index', project_id=project.id)
-    else:
-        form = TaskForm(project=project)
-    return render(request, 'tasks/form.html', {
-        'project': project, 
-        'form': form, 
-        'action': 'Add'
-    })
-
-@login_required
-def edit_task(request, project_id, task_id):
-    project = get_object_or_404(Project, id=project_id, user=request.user)
-    task = get_object_or_404(Task, id=task_id, project=project)
-    if request.method == 'POST':
-        form = TaskForm(request.POST, instance=task, project=project)
-        if form.is_valid():
-            form.save()
-            return redirect('task_detail', project_id=project.id, task_id=task.id)
-    else:
-        form = TaskForm(instance=task, project=project)
-    return render(request, 'tasks/form.html', {
-        'project': project, 
-        'form': form, 
-        'action': 'Edit', 
-        'task': task
-    })
-
-@login_required
-def delete_task(request, project_id, task_id):
-    project = get_object_or_404(Project, id=project_id, user=request.user)
-    task = get_object_or_404(Task, id=task_id, project=project)
-    if request.method == 'POST':
-        task.delete()
-        return redirect('tasks_index', project_id=project.id)
-    return render(request, 'tasks/delete.html', {
-        'project': project, 
-        'task': task
-    })
